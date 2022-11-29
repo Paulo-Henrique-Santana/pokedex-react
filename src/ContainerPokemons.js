f350b0ee4953fc61136f261fddb6bce2e980c64e
Paulo-Henrique-Santana <phenrique.santana@hotmail.com>
2022-11-29 19:16:32 -0300

    Adiciona mensagem de carregamento e altera fonte e cores

--- src/ContainerPokemons.js
+++ src/ContainerPokemons.js
@@ -5,8 +5,10 @@ import "./css/ContainerPokemons.css";
 
 const ContainerPokemons = ({ urlPokemons }) => {
   const [pokemons, setPokemons] = React.useState(null);
+  const [msgCarregando, setMsgCarregando] = React.useState(null);
 
   React.useEffect(() => {
+    setMsgCarregando("Carregando...");
     const buscarPokemons = async () => {
       const dadosPokemons = await Promise.all(
         urlPokemons.map(async ({ url }) => {
@@ -16,12 +18,14 @@ const ContainerPokemons = ({ urlPokemons }) => {
         })
       );
       setPokemons(dadosPokemons);
+      setMsgCarregando(null);
     };
     buscarPokemons();
   }, []);
 
   return (
     <main className="container-pokemons">
+      {msgCarregando && <p className="msg-carregando">{msgCarregando}</p>}
       {pokemons &&
         pokemons.map((dados) => <CardPokemon key={dados.id} dados={dados} />)}
     </main>
