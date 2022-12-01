2c8a65d5410e66eff327042312d5a0a229e46bc2
Paulo-Henrique-Santana <phenrique.santana@hotmail.com>
2022-11-30 22:42:42 -0300

    Altera o tamanho e deixa em maíusculas algumas fontes

--- src/CardPokemon.js
+++ src/CardPokemon.js
@@ -4,8 +4,8 @@ import "./css/CardPokemon.css";
 
 const CardPokemon = ({ dados: { name, types }, urlImg }) => {
   return (
-    <div className="container-pokemon">
-      <h1>{name}</h1>
+    <div className="card-pokemon">
+      <h1 className="name">{name.replace(name[0], name[0].toUpperCase())}</h1>
 
       <div className="box-image">
         <img src={urlImg} alt={name} />
